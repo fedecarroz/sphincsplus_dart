d1126b9ec7caeada33f34ef9b5c92d8b2b16562d
Federico Carrozzino <federicocarrozzino98@gmail.com>
2023-11-21 17:59:41 +0100

    fix dependency

--- lib/src/helpers/ffi_run_func.dart
+++ lib/src/helpers/ffi_run_func.dart
@@ -3,7 +3,7 @@ import 'dart:ffi';
 typedef LongLongFunc = LongLong Function();
 typedef DartIntFunc = int Function();
 
-int ffiRunIntFunc(lib, fnName) {
+int ffiRunIntFunc(DynamicLibrary lib, String fnName) {
   final fn = lib.lookupFunction<LongLongFunc, DartIntFunc>(fnName);
   return fn();
 }
