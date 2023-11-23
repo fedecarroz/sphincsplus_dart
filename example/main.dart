dc54f91e77e3604c592750642d7fbb2cca3cc0a2
Federico Carrozzino <federicocarrozzino98@gmail.com>
2023-11-23 03:49:33 +0000

    example started

--- example/main.dart
+++ example/main.dart
@@ -1,3 +1,17 @@
+import 'dart:typed_data';
+
+import 'package:sphincsplus/sphincsplus_dart.dart';
+
 void main() {
-  print('Hello World!');
+  final spxSigner = SpxSigner(params: Params.haraka_128f);
+
+  print('PK len: ${spxSigner.pkLength}');
+  print('SK len: ${spxSigner.skLength}');
+  print('Seed len: ${spxSigner.seedLength}');
+  print('Signature len: ${spxSigner.signatureLength}');
+
+  Uint8List pk, sk;
+  (pk, sk) = spxSigner.generateKeyPair(seed: 16);
+  print('Public key: $pk');
+  print('Secret key: $sk');
 }
